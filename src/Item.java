4781b041a32c0fb2be03eb3799947476d7c2def2
hendy79 <hendyalgorithm@programmer.net>
2022-12-20 15:17:49 +0700

    feat: 1st release

---
+++ src/Item.java
@@ -0,0 +1,25 @@
+
+public class Item {
+	private String name;
+	private int price;
+	private int stock;
+	
+	public Item(String name, int price, int stock) {
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+}
